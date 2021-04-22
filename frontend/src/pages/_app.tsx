6babaeb0b09ce37f88ce29279a04950c92129e76
pipo2tr <siddharthpipo2tr@gmail.com>
2021-04-22 19:17:22 +0530

    switch to apollo and add ssr

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { CssBaseline } from "@material-ui/core";
 import Head from "next/head";
 import React, { useEffect } from "react";
@@ -13,19 +12,13 @@ function MyApp({ Component, pageProps }) {
 		}
 	}, []);
 
-	const client = new ApolloClient({
-		uri: "http://localhost:5000/graphql",
-		cache: new InMemoryCache(),
-		credentials: "include"
-	});
 	return (
-		<>	<ApolloProvider client={client}>
+		<>
 			<Head>
 				<title>ThinkIt!</title>
 			</Head>
 			<CssBaseline />
 			<Component {...pageProps} />
-			</ApolloProvider>
 		</>
 	);
 }

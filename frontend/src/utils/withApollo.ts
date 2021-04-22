05c04c9370c48c974c16a2bb65eafc965f2fcbcd
pipo2tr <siddharthpipo2tr@gmail.com>
2021-04-22 19:14:26 +0530

    add next-apollo and caching

---
+++ frontend/src/utils/withApollo.ts
@@ -0,0 +1,37 @@
+import { withApollo as createWithApollo } from "next-apollo";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { NextPageContext } from "next";
+import {PaginatedPost} from "../generated/graphql"
+const createClient = (ctx: NextPageContext) =>
+	new ApolloClient({
+		uri: "http://localhost:5000/graphql",
+		credentials: "include",
+		headers: {
+			cookie:
+				typeof window == "undefined" ? ctx?.req?.headers?.cookie : "",
+		},
+		cache: new InMemoryCache({
+			typePolicies: {
+				Query: {
+					fields: {
+						posts: {
+							keyArgs: [],
+							merge(
+								existing: PaginatedPost | undefined,
+								incoming: PaginatedPost
+							): PaginatedPost {
+								return {
+									...incoming,
+									posts: [
+										...(existing?.posts || []),
+										...incoming.posts,
+									],
+								};
+							},
+						},
+					},
+				},
+			},
+		}),
+	});
+export const withApollo = createWithApollo(createClient);

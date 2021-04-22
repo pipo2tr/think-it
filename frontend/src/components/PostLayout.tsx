ccdb5ee72e8a31fe4b4de1ef4e9711cf0f687053
pipo2tr <siddharthpipo2tr@gmail.com>
2021-04-22 19:16:11 +0530

    update layout

---
+++ frontend/src/components/PostLayout.tsx
@@ -0,0 +1,61 @@
+import { Container, useMediaQuery } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import React, { FC } from "react";
+import { HasAuthMobile } from "../components/HasAuth";
+import Layout from "../components/Layout";
+import { useMeQuery } from "../generated/graphql";
+
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		paper: {
+			marginTop: theme.spacing(8),
+			display: "flex",
+			flexDirection: "row",
+			justifyContent: "center",
+			width: "100%",
+		},
+
+		grid1: {
+			minWidth: "360px",
+		},
+		grid2: {
+			display: "none",
+			[theme.breakpoints.up("md")]: {
+				display: "block",
+			},
+		},
+		ig: {
+			position: "sticky",
+			top: theme.spacing(9),
+		},
+	})
+);
+
+const PostLayout: FC = ({ children }) => {
+	const classes = useStyles();
+	const { data } = useMeQuery();
+	const mq = useMediaQuery('(max-width:600px)')
+	
+	
+	return (
+		<Layout>
+			<Container component="main" maxWidth={mq ? "xl" : "md"}>
+				<Grid container spacing={mq ? 0 : 3} className={classes.paper}>
+					<Grid item xs={9} className={classes.grid1}>
+						{children}
+					</Grid>
+					{data?.me ? (
+						<Grid item xs={3} className={classes.grid2}>
+							<Paper className={classes.ig}>
+								<HasAuthMobile />
+							</Paper>
+						</Grid>
+					) : null}
+				</Grid>
+			</Container>
+		</Layout>
+	);
+};
+export default PostLayout;

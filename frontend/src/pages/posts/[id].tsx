4ad94ed9cf90d3ed2931e3fa0b83b0a48c54e48b
pipo2tr <siddharthpipo2tr@gmail.com>
2021-04-22 19:19:41 +0530

    view single post

---
+++ frontend/src/pages/posts/[id].tsx
@@ -0,0 +1,29 @@
+import { useRouter } from "next/router";
+import React from "react";
+import PostCards from "../../components/PostCards";
+import PostLayout from "../../components/PostLayout";
+import { usePostQuery } from "../../generated/graphql";
+import { withApollo } from "../../utils/withApollo";
+
+const post = () => {
+	const router = useRouter();
+	const Id = parseInt(router.query.id as string);
+	const { data, loading } = usePostQuery({
+		skip: !Id,
+		variables: {
+			id: Id,
+		},
+	});
+
+	return (
+		<PostLayout>
+			{data?.post ? (
+				<PostCards post={data.post} />
+			) : (
+				<div>Post doesn't exist</div>
+			)}
+		</PostLayout>
+	);
+};
+
+export default withApollo({ ssr: true })(post);

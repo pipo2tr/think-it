c1707e2cb0c81135d218df0786fc2e0cd69fce8d
pipo2tr <siddharthpipo2tr@gmail.com>
2021-04-23 16:55:34 +0530

    add favicons

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,66 +1,90 @@
-import React from 'react';
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { ServerStyleSheets } from '@material-ui/core/styles';
-
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheets } from "@material-ui/core/styles";
 
 export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-                />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+	render() {
+		return (
+			<Html lang="en">
+				<Head>
+					<link
+						rel="shortcut icon"
+						href="/favicon.ico"
+						type="image/x-icon"
+					/>
+					<link
+						rel="apple-touch-icon"
+						sizes="180x180"
+						href="/apple-touch-icon.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="32x32"
+						href="/favicon-32x32.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="16x16"
+						href="/favicon-16x16.png"
+					/>
+					<link
+						rel="stylesheet"
+						href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
 
 // `getInitialProps` belongs to `_document` (instead of `_app`),
 // it's compatible with server-side generation (SSG).
 MyDocument.getInitialProps = async (ctx) => {
-  // Resolution order
-  //
-  // On the server:
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. document.getInitialProps
-  // 4. app.render
-  // 5. page.render
-  // 6. document.render
-  //
-  // On the server with error:
-  // 1. document.getInitialProps
-  // 2. app.render
-  // 3. page.render
-  // 4. document.render
-  //
-  // On the client
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. app.render
-  // 4. page.render
+	// Resolution order
+	//
+	// On the server:
+	// 1. app.getInitialProps
+	// 2. page.getInitialProps
+	// 3. document.getInitialProps
+	// 4. app.render
+	// 5. page.render
+	// 6. document.render
+	//
+	// On the server with error:
+	// 1. document.getInitialProps
+	// 2. app.render
+	// 3. page.render
+	// 4. document.render
+	//
+	// On the client
+	// 1. app.getInitialProps
+	// 2. page.getInitialProps
+	// 3. app.render
+	// 4. page.render
 
-  // Render app and page and get the context of the page with collected side effects.
-  const sheets = new ServerStyleSheets();
-  const originalRenderPage = ctx.renderPage;
+	// Render app and page and get the context of the page with collected side effects.
+	const sheets = new ServerStyleSheets();
+	const originalRenderPage = ctx.renderPage;
 
-  ctx.renderPage = () =>
-    originalRenderPage({
-      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-    });
+	ctx.renderPage = () =>
+		originalRenderPage({
+			enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+		});
 
-  const initialProps = await Document.getInitialProps(ctx);
+	const initialProps = await Document.getInitialProps(ctx);
 
-  return {
-    ...initialProps,
-    // Styles fragment is rendered after the app and page rendering finish.
-    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
-  };
+	return {
+		...initialProps,
+		// Styles fragment is rendered after the app and page rendering finish.
+		styles: [
+			...React.Children.toArray(initialProps.styles),
+			sheets.getStyleElement(),
+		],
+	};
 };

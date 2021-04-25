ee4cfdf0978b38441b4e850d55b41af152f99f14
pipo2tr <siddharthpipo2tr@gmail.com>
2021-04-25 12:26:39 +0530

    check if user is logged in

--- frontend/src/pages/create-post.tsx
+++ frontend/src/pages/create-post.tsx
@@ -10,6 +10,7 @@ import { useRouter } from "next/router";
 import React from "react";
 import Layout from "../components/Layout/Layout";
 import { useCreatePostMutation } from "../generated/graphql";
+import { useIsAuth } from "../utils/useisAuth";
 import { withApollo } from "../utils/withApollo";
 const useStyles = makeStyles((theme) => ({
 	paper: {
@@ -32,6 +33,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const createPost = () => {
+	useIsAuth()
 	const [createPost, { loading }] = useCreatePostMutation();
 	const router = useRouter();
 	const classes = useStyles();
